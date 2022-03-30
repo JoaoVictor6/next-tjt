cf106e6adcc0c0a672c328eee6dd27d91265720d
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2022-03-30 12:03:07 -0300

    feat: apply example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import Head from 'next/head'
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    axios.get('/api/reminders').then(res => {
+      console.log(res.data);
+    });
+  }, []);
   return (
     <>
       <h1 className='text-4xl'>Home</h1>
     </>
-  )
+  );
 }
